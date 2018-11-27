e982b3dd325245e6217b9d980b32f5b5d2d817e4
Aga <a.dufrat@gmail.com>
2018-11-27 17:02:27 +0000

    Energy usage

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -74,5 +74,33 @@ it ('can be reset to the default temperature', function(){
   expect(thermostat.getCurrentTemperature()).toEqual(20);
 });
 
+describe('displaying usage levels', function() {
+  describe('when the pemperature is below 18 degrees', function() {
+    it('is considered low-usage', function() {
+      for (var i = 0; i < 3; i++) {
+        thermostat.down();
+      }
+      expect(thermostat.energyUsage()).toEqual('low-usage');
+    });
+  });
+
+  describe('when the temperature is between 18 and 25 degrees', function() {
+    it('is considered medium-usage', function() {
+      expect(thermostat.energyUsage()).toEqual('medium-usage');
+    });
+  });
+
+  describe('when the temperature is anything else', function() {
+    it('is considered high-usage', function() {
+      thermostat.PowerSavingMode = false;
+      for (var i = 0; i < 6; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.energyUsage()).toEqual('high-usage');
+    });
+  });
+
+});
+
 
 });
