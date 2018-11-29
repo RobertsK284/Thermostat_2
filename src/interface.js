757baacecb696af11f4c21538468d76e5bb6d205
Karen Roberts <karenroberts@Karens-MacBook-Pro.local>
2018-11-29 14:38:44 +0000

    added Current temperature and city select feature

--- src/interface.js
+++ src/interface.js
@@ -2,6 +2,27 @@ $(document).ready(function() {
   var thermostat = new Thermostat();
   updateTemperature();
 
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5f45fe47bd823c145af9085e89e371b1&units=metric', function(data) {
+  $('#current-temperature').text(data.main.temp);
+})
+
+function displayWeather(city) {
+  var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+  var token = '&appid=5f45fe47bd823c145af9085e89e371b1';
+  var units = '&units=metric';
+  $.get(url + token + units, function(data) {
+    $('#current-temperature').text(data.main.temp);
+  })
+}
+
+displayWeather('London');
+
+$('#select-city').submit(function(event) {
+  event.preventDefault();
+  var city = $('#current-city').val();
+  displayWeather(city);
+})
+
 $('#temperature-up').on('click', function() { // event listener
   thermostat.up(); // update model
   updateTemperature(); // update view
